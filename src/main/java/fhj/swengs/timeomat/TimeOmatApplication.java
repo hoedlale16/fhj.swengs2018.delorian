26d971f51e11c7e91595a7648c38acac4210042d
Alexander H <alexander.hoedl@edu.fh-joanneum.at>
2018-11-06 10:44:51 +0100

    initial creation of swengs project

---
+++ src/main/java/fhj/swengs/timeomat/TimeOmatApplication.java
@@ -0,0 +1,12 @@
+package fhj.swengs.timeomat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeOmatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeOmatApplication.class, args);
+	}
+}
---
+++ src/test/java/fhj/swengs/timeomat/TimeOmatApplicationTests.java
@@ -0,0 +1,16 @@
+package fhj.swengs.timeomat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimeOmatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

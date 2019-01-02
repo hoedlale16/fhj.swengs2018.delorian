5ce472903303dcc9db101647861cebf4982e9d8c
MrPink1992 <maximilian.steiner@edu.fh-joanneum.at>
2019-01-02 16:45:07 +0100

    ..

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {Router} from '@angular/router';
-
+import {User} from '../../api/User';
+import {Project} from '../../api/Project';
+import {UserService} from '../../services/user.service';
+import {ProjectService} from '../../services/project.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
   currLoggedInUser: string;
 
   constructor(private authService: AuthService) { }
@@ -17,3 +19,4 @@ export class DashboardComponent implements OnInit {
     this.currLoggedInUser = this.authService.currLoggedInUserName;
   }
 }
+

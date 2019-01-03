4d93c8e9a8756ce90229f273d8e1d1de0258f3e9
Alexander H <alexander.hoedl@edu.fh-joanneum.at>
2019-01-03 15:14:26 +0100

    colapse of divs is working now

--- frontend/src/app/components/project-info/project-info.component.ts
+++ frontend/src/app/components/project-info/project-info.component.ts
@@ -13,51 +13,38 @@ export class ProjectInfoComponent implements OnInit {
 
   @Input() project: Project;
   @Input() showFullDetails = false;
-
   @Input() buttonLink: string;
   @Input() buttonLinkName: string;
-
-  @Input()
-  totalBookedHours = 0;
+  @Input() totalBookedHours = 0;
 
   chartData: any[] = [];
-  options: any;
-  maxValueChart: any;
-
-  chartColors: Array<any> = [
-    {
-      backgroundColor: ['#fe59c2', '#9559fe', '#c2fe59' , '#59fe95' , '#00ffff', '#FF0000']
-    }];
-
+  chartOptions: any;
 
   constructor(private projectService: ProjectService, private router: Router) {
-
   }
 
-
   ngOnInit() {
+    this.prepareChart();
+  }
 
-    this.maxValueChart = this.project.totalPlannedHours > this.totalBookedHours ? this.project.totalPlannedHours : this.totalBookedHours;
-
+  prepareChart() {
+    const maxValueChart = this.project.totalPlannedHours > this.totalBookedHours ? this.project.totalPlannedHours : this.totalBookedHours;
     this.chartData = [
       {data: [this.project.totalPlannedHours], label: 'Planned Hours'},
-      {data: [this.totalBookedHours], label: 'Worked Hours'}
-    ];
-    this.options = {
+      {data: [this.totalBookedHours], label: 'Worked Hours'} ];
+
+    this.chartOptions = {
       scales: {
         yAxes: [{
           ticks: {
-            max: this.maxValueChart,
+            max: maxValueChart,
             min: 0,
-            stepSize: this.maxValueChart / 10
+            stepSize: maxValueChart / 10
           }
         }]
       }
     };
-
-
   }
-
 
   deactivateProject(project: Project) {
 

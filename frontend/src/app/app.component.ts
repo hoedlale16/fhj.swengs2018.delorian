9dc03fc61c720b0cb4480f8224d12b9f1a40beb1
MrBrown1992 <nikolaus.spiess@edu.fh-joanneum.at>
2019-01-16 09:54:47 +0100

    wurscht

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Title} from '@angular/platform-browser';
+import {AuthService} from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,24 @@ import {Title} from '@angular/platform-browser';
 })
 export class AppComponent {
   title = 'Delorian - Track your time!';
+  background =
 
-  public constructor(private titleService: Title ) {
+  public constructor(private titleService: Title, private authService: AuthService) {
     this.setTitle(this.title);
   }
 
   public setTitle( newTitle: string) {
     this.titleService.setTitle(newTitle);
   }
+
+
+  ngOnInit() {
+    this.authService.loggedInChange.subscribe((isLoggedIn) => {
+     if (isLoggedIn) {
+       return;
+     }
+    });
+  }
+
+
 }
